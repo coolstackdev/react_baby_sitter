119adb70c791ab8abbda221800be5ba6a290b09c
CoolFullstackDev <topace1029@hotmail.com>
2019-01-23 23:33:31 +0300

    before use saga

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store/store';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <div className="App">
+            <PostForm />
+            <hr />
+            <Posts />
+        </div>
+    </Provider>,
+    document.getElementById('root'),
+);
+
 serviceWorker.unregister();
