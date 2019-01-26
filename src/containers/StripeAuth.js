f4d409245e305047443a37521fedf41e26b41c2d
CoolFullstackDev <topace1029@hotmail.com>
2019-01-27 01:42:05 +0300

    heroku config bug fix

--- src/containers/StripeAuth.js
+++ src/containers/StripeAuth.js
@@ -15,26 +15,25 @@ export default class StripeAuth extends Component {
 
     componentDidMount() {
 
+        // get query string from redirected uri
         const search = this.props.location.search;
         const params = new URLSearchParams(search);
         const code = params.get('code');
         const state = params.get('state');
 
-        console.log(code + "," + state);
-
-        // var session = sessionService.loadSession();
-        // console.log(session);
-
+        // get session and check if two state matched
         sessionService.loadSession().then(result => {
+
             if (result.status == state) {
                 this.state.isMatched = true;
                 console.log('status key is matched');
 
+                // if matched, send second request to stripe for getting stripe account id for Babysitters
                 var params = {
                     form: {
                         grant_type: 'authorization_code',
-                        client_id: config.stripe.clientId,
-                        client_secret: config.stripe.secretKey,
+                        client_id: config.clientId,
+                        client_secret: config.secretKey,
                         code: code
                     },
                     json: true
