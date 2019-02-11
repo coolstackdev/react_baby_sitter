9024d77a3acb4f9d638084b3f8614ccc27615200
CoolFullstackDev <topace1029@hotmail.com>
2019-02-11 08:28:25 +0300

    remove string

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,7 +26,7 @@ const Home = ({
                 <div className="intro">
                     <div className="pilots">
                         <h1>Get ready</h1>
-                        <h2>Lightning Bug Babysitter is the world’s leading platform. <br /> Join our elite team of Lightning Bug Babysitters.</h2>
+                        <h2>Join our elite team of Lightning Bug Babysitters.</h2>
                         <br />
                     </div>
                 </div>
